c612145ad24db131b769e831e2a633c3e38da035
aetox <core.editor>
2023-04-13 21:25:47 +0200

    Version finalee

--- app/src/main/java/com/example/equides/InterfaceActivity.java
+++ app/src/main/java/com/example/equides/InterfaceActivity.java
@@ -1,15 +1,20 @@
 package com.example.equides;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
@@ -17,6 +22,8 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -24,71 +31,59 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.FileProvider;
-
-import android.os.Bundle;
-import android.os.Environment;
-import android.widget.Toast;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 
-import android.content.ActivityNotFoundException;
-
-
-
-
 public class InterfaceActivity extends AppCompatActivity {
 
     private TextView welcomeMailTextView;
-
     private String email;
-
     private Button PDFBtn;
     private DatabaseManager databaseManager;
-
     private String id_detenteur;
 
-    private Toolbar toolbar;
+    private static final int REQUEST_STORAGE_PERMISSION = 100;
 
+    private void requestStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_STORAGE_PERMISSION);
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults); // Ajoutez cet appel à la méthode super
+        if (requestCode == REQUEST_STORAGE_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // La permission a été accordée, vous pouvez maintenant télécharger et sauvegarder le PDF.
+            } else {
+                Toast.makeText(this, "Permission refusée. Vous ne pourrez pas télécharger le PDF.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interface);
 
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
 
         welcomeMailTextView = findViewById(R.id.welcomeMailTextView);
         PDFBtn = findViewById(R.id.PDFBtn);
 
         id_detenteur = getIntent().getStringExtra("idDetenteur");
-
         email = getIntent().getStringExtra("mail");
-
         welcomeMailTextView.setText(email);
-
-
-
 
         PDFBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 String url = "https://equides.eu/API_Equides/getPDF.php";
+                requestStoragePermission();
                 downloadPdf(url);
             }
         });
-
     }
 
     private void downloadPdf(String url) {
@@ -98,7 +93,7 @@ public class InterfaceActivity extends AppCompatActivity {
         urlBuilder.addQueryParameter("idDetenteur", id_detenteur);
         String urlWithUserId = urlBuilder.build().toString();
 
-                okhttp3.Request request = new okhttp3.Request.Builder()
+        okhttp3.Request request = new okhttp3.Request.Builder()
                 .url(urlWithUserId)
                 .build();
 
@@ -124,33 +119,18 @@ public class InterfaceActivity extends AppCompatActivity {
         });
     }
 
-
-
     private void savePdf(byte[] bytes) {
+        Date now = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+        String dateStr = format.format(now);
 
-        // Créer un objet Date avec la date du jour
-                Date now = new Date();
-
-        // Créer un objet SimpleDateFormat pour formater la date
-                SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
-
-        // Formater la date en une chaîne de caractères
-                String dateStr = format.format(now);
-
-        File pdfFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "Carnet_Transport_du_"+ dateStr +".pdf");
+        File pdfFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "Carnet_Transport_du_" + dateStr + ".pdf");
         try {
             FileOutputStream fos = new FileOutputStream(pdfFile);
             fos.write(bytes);
             fos.close();
 
-            // Créez une Uri sécurisée à partir du fichier PDF
             Uri pdfUri = FileProvider.getUriForFile(this, "com.example.equides.fileprovider", pdfFile);
-
-            // Démarrer une intent pour afficher le fichier PDF
-            Intent pdfIntent = new Intent(Intent.ACTION_VIEW);
-            pdfIntent.setDataAndType(pdfUri, "application/pdf");
-            pdfIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            startActivity(pdfIntent);
 
             runOnUiThread(new Runnable() {
                 @Override
@@ -160,38 +140,30 @@ public class InterfaceActivity extends AppCompatActivity {
             });
         } catch (IOException e) {
             e.printStackTrace();
+            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
         }
     }
 
-
-
-    public void onApiResponse(JSONObject response){
+    public void onApiResponse(JSONObject response) {
         Boolean success = null;
-        String error ="";
+        String error = "";
         String idLogin = "";
-
         try {
             success = response.getBoolean("success");
 
-            if (success){
-
-                idLogin = response.getString("id_login"); // Recupere l'id_login de la personne connectée
-
-
-            }else {
+            if (success) {
+                idLogin = response.getString("id_login"); // Récupère l'id_login de la personne connectée
+            } else {
                 error = response.getString("error");
             }
 
-        }catch (JSONException e){
+        } catch (JSONException e) {
             e.printStackTrace();
             Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
-
         }
-
     }
 
     public void displayUserInfo() {
-
         // Envoie la requête POST
         String url = "http://10.0.2.2/API_Equides/connectUser.php";
 
@@ -216,6 +188,5 @@ public class InterfaceActivity extends AppCompatActivity {
         });
 
         databaseManager.queue.add(jsonObjectRequest);
-
     }
 }
